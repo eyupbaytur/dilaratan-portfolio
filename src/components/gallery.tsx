3d447df16fc86ed87266f1383314f4f8336e6389
eyupbaytur <eyupbaytur@gmail.com>
2024-07-05 19:14:19 +0300

    added images and masonry grid for gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -3,43 +3,94 @@ import Image from 'next/image';
 
 export default function Gallery() {
     return (
-        <div className=" grid grid-cols-2 md:grid-cols-3 gap-[1%] p-[5%]">
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt=""/>
+        <div>
+            <div className=" grid grid-cols-2 md:grid-cols-3 gap-[1%] p-[5%]">
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/1.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/2.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/3.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/4.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/5.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/6.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/6.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/7.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/8.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/9.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/10.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/11.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+        </div>
+{/*
+        <div className='p-20'>--------------AYRIM------------</div>
+        <div className=" grid grid-cols-3 md:grid-cols-3 gap-[1%] p-[5%]">
+            <div className="grid gap-4">
+                <div className='position: "fixed"'>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/1.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/2.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/3.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
             </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt=""/>
+            <div className="grid gap-4">
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/4.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/5.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/6.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
+                </div>
             </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt=""/>
+            <div className="grid gap-4">
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/7.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/8.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/9.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
+                </div>
             </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt=""/>
+            <div className="grid gap-4">
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/10.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/11.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
+                </div>
+                <div>
+                    <Image className="h-auto max-w-full rounded-lg" src="/assets/landing-bg3.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                </div>
             </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt=""/>
-            </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt=""/>
-            </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt=""/>
-            </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt=""/>
-            </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt=""/>
-            </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt=""/>
-            </div>
-            <div>
-                <img className="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt=""/>
-            </div>
-            <div>
-                <img className="h-auto max-w-full" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt=""/>
-            </div>
+        </div>
+*/}
     </div>
     )
 };
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Hero() {
     return (
         <div className='absolute flex justify-center items-center'>
-            <Image className='object-scale-down md:object-cover ' src="/assets/landing-bg1.jpg" alt="" quality={100} width={0} height={0} sizes="500vw" style={{ width: 'auto', height: 'auto' }}/>
+            <Image className='object-scale-down md:object-cover ' src="/assets/landing-bg1.jpg" alt="" quality={75} width={0} height={0} sizes="500vw" style={{ width: 'auto', height: 'auto' }}/>
         </div>
     )
 };
