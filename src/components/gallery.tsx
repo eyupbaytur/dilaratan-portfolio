9dbd09325161aebe241d64ed97585f94b3875ba1
eyupbaytur <eyupbaytur@gmail.com>
2024-07-08 18:12:49 +0300

    fixed gallery component masonry grid

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -3,94 +3,33 @@ import Image from 'next/image';
 
 export default function Gallery() {
     return (
-        <div>
-            <div className=" grid grid-cols-2 md:grid-cols-3 gap-[1%] p-[5%]">
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/1.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+            <div className="grid grid-cols-2 md:grid-cols-2 gap-3 md:gap-14 p-10 md:p-20">
+                <div className="grid-flow-row ">
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/1.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/2.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/3.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/4.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/5.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/6.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
                 </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/2.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/3.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/4.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/5.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/6.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/7.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/8.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/9.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/10.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/11.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" blurDataURL='/assets/portfolio-images/1.jpg' placeholder='blur' src="/assets/portfolio-images/1.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-        </div>
-{/*
-        <div className='p-20'>--------------AYRIM------------</div>
-        <div className=" grid grid-cols-3 md:grid-cols-3 gap-[1%] p-[5%]">
-            <div className="grid gap-4">
-                <div className='position: "fixed"'>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/1.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/2.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/3.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                <div className="grid-flow-row gap-3 md:gap-14">
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/7.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/8.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/9.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/10.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                    
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/11.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                    
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/12.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
                 </div>
             </div>
-            <div className="grid gap-4">
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/4.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/5.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/6.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
-                </div>
-            </div>
-            <div className="grid gap-4">
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/7.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/8.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/9.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
-                </div>
-            </div>
-            <div className="grid gap-4">
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/10.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/portfolio-images/11.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75}  alt=""/>
-                </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/assets/landing-bg3.jpg"style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
-                </div>
-            </div>
-        </div>
-*/}
-    </div>
     )
 };
