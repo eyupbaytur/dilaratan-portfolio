19c061f016e6a2dc1c1eac9925d885232448fc78
eyupbaytur <eyupbaytur@gmail.com>
2024-07-10 01:19:06 +0300

    fixed hamburger menu padding issue

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -5,17 +5,17 @@ export default function Gallery() {
     return (
             <div className="grid grid-cols-2 md:grid-cols-2 gap-3 md:gap-14 py-10 md:py-14 px-10 md:px-20">
                 <div className="grid-flow-row ">
-                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/1.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/1.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
 
-                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/2.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/2.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
 
-                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/3.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/3.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
 
-                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/4.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/4.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
 
-                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/5.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/5.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
 
-                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/6.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
+                    <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/6.jpg" style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
                 </div>
                 <div className="grid-flow-row gap-3 md:gap-14">
                     <Image className="h-auto max-w-full mb-3 md:mb-14" src="/assets/portfolio-images/7.jpg"loading='lazy' style={{ width: '100%', height: 'auto' }} sizes="100vw" width={0} height={0} quality={75} alt=""/>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,38 +2,41 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Hamburger from '@/components/hamburger';
+import dynamic from 'next/dynamic'
 
 export default function Navbar() {
     return (
         <div className='sticky w-full h-20 z-[100] text-base text-gray-800 font-oswaldfont px-10 md:px-20 py-10 md:py-14'>
-            <div className='flex justify-between items-center w-full h-full'>
-                <Link href='/#Home'>
-                    <ul className=' text-base uppercase '>DILARA TAN</ul>
-                </Link>
-            <div className='text-base uppercase flex justify-between items-center '>
-                <ul className='hidden md:flex '>
-                    <Link href='/portfolio'>
-                        <li className=''>PORTFOLIO</li>
+            <div>
+                <div className='flex justify-between items-center w-full h-full'>
+                    <Link href='/#Home'>
+                        <ul className=' md:left-auto left-10 text-base uppercase '>DILARA TAN</ul>
                     </Link>
-                    <Link href='/projects'>
-                        <li className='ml-10 '>PROJECTS</li>
-                    </Link>
-                    <Link href='/about'>
-                        <li className='ml-10 '>ABOUT</li>
-                    </Link>
-                    <Link href='/contact'>
-                        <li className='ml-10 '>CONTACT</li>
-                    </Link>
-                    <Link className='ml-10 relative h-6 w-6' target="_blank" href='https://www.instagram.com/dilaratan0/'>
-                        <li className=''><Image src="/assets/instagram-logo.png" alt="" objectPosition='bottom' fill/></li>
-                    </Link>
-                </ul>
-                <div className='md:hidden cursor-pointer'>
-                    <Hamburger/>
+                <div className='text-base uppercase flex justify-between items-center '>
+                    <ul className='hidden md:flex '>
+                        <Link href='/portfolio'>
+                            <li className=''>PORTFOLIO</li>
+                        </Link>
+                        <Link href='/projects'>
+                            <li className='ml-10 '>PROJECTS</li>
+                        </Link>
+                        <Link href='/about'>
+                            <li className='ml-10 '>ABOUT</li>
+                        </Link>
+                        <Link href='/contact'>
+                            <li className='ml-10 '>CONTACT</li>
+                        </Link>
+                        <Link className='ml-10 relative h-6 w-6' target="_blank" href='https://www.instagram.com/dilaratan0/'>
+                            <li className=''><Image src="/assets/instagram-logo.png" alt="" loading="eager" objectPosition='bottom' fill/></li>
+                        </Link>
+                    </ul>
+                    <div className='md:hidden cursor-pointer'>
+                        <Hamburger/>
+                    </div>
                 </div>
             </div>
-            <div className='md:hidden left-0 top-0 w-full h-screen bg-black/70'>
-                <div className='fixed left-0 top-0 w-[100%] sm:w-[100%] md:w-[45%] h-screen bg-[#ffffff] p-10 ease-in duration-500'>
+            <div className='md:hidden left-0 top-0 w-full h-screen'>
+                <div className='hidden left-0 top-0 w-[100%] sm:w-[100%] md:w-[45%] h-screen bg-[#ffffff] p-10 ease-in duration-500'>
                     <div className=''>
                         <div className='flex w-full items-center justify-between'>
                             <div>
